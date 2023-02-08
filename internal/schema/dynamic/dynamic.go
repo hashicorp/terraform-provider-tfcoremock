4ee85294498cc2869d0f9eb846d2cf07b542a3e9
Liam Cervante <liam.cervante@hashicorp.com>
2023-02-08 10:33:59 +0100

    Improve error messages around dynamic resources (#36)
    * improve error messages around dynamic resources
    
    * use embed instead of fetching from github

--- internal/schema/dynamic/dynamic.go
+++ internal/schema/dynamic/dynamic.go
@@ -5,9 +5,15 @@ package dynamic
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/xeipuuv/gojsonschema"
 
 	"github.com/hashicorp/terraform-provider-tfcoremock/internal/schema"
+	jsonschema "github.com/hashicorp/terraform-provider-tfcoremock/schema"
 )
 
 type Reader interface {
@@ -23,22 +29,42 @@ type StringReader struct {
 }
 
 func (r FileReader) Read() (map[string]schema.Schema, error) {
+	schemaLoader := gojsonschema.NewStringLoader(jsonschema.DynamicResourcesJsonSchema)
+
 	data, err := os.ReadFile(r.File)
 	if err != nil {
+		// TODO(liamcervante): It's okay if there is no dynamic_resources.json
+		//   file, but if the user has set the environment variable changing the
+		//   location maybe we should complain about it?
 		if os.IsNotExist(err) {
 			return nil, nil
 		}
+		return nil, errors.Wrap(err, "failed to read dynamic resources file")
+	}
+
+	documentLoader := gojsonschema.NewStringLoader(string(data))
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
 		return nil, err
 	}
 
-	var dynamicResources map[string]schema.Schema
-	if len(data) > 0 {
-		if err := json.Unmarshal(data, &dynamicResources); err != nil {
-			return nil, err
+	if result.Valid() {
+		var dynamicResources map[string]schema.Schema
+		if len(data) > 0 {
+			if err := json.Unmarshal(data, &dynamicResources); err != nil {
+				return nil, errors.Wrap(err, "failed to unmarshal dynamic resources json")
+			}
 		}
+
+		return dynamicResources, nil
 	}
 
-	return dynamicResources, nil
+	var errs []string
+	for _, err := range result.Errors() {
+		errs = append(errs, err.String())
+	}
+
+	return nil, fmt.Errorf("failed json schema check: %s", strings.Join(errs, ", "))
 }
 
 func (r StringReader) Read() (map[string]schema.Schema, error) {
